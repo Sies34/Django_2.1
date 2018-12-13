e700cb41a1f8f9f27ef4fc05b7cd156bc01fdc2e
Johan Kuepers <Johan.Kuepers@jacobs.com>
2018-12-13 20:11:43 +0100

    Page 107

--- mfdw_root/pages/admin.py
+++ mfdw_root/pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Page
 
-# Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('tittle', 'update_date')
+    ordering = ('tittle',)
+    search_fields = ('tittle',)
+
+admin.site.register(Page, PageAdmin)
