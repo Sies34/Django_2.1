36432e25a645076ed8d82160b0fc5f369be72d13
Johan Kuepers <johan.kuepers@jacobs.com>
2018-12-13 15:30:17 +0100

    First setup

---
+++ env_mfdw/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\kueperj\pycharmprojects\django_2.1\env_mfdw\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
