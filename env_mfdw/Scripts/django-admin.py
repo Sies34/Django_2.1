4905affb76f424a652e542f4b95ca061bf038382
jkuep <jkuepers@hotmail.com>
2018-12-13 18:04:05 +0100

    Page 88

--- env_mfdw/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\kueperj\pycharmprojects\django_2.1\env_mfdw\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
