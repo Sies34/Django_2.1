ebafeebe16c8c0eb88878583b0cc74d8dd39b517
Johan Kuepers <Johan.Kuepers@jacobs.com>
2018-12-17 19:54:35 +0100

    Page 130

--- mfdw_root/pages/admin.py
+++ mfdw_root/pages/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Page
 
 class PageAdmin(admin.ModelAdmin):
-    list_display = ('tittle', 'update_date')
-    ordering = ('tittle',)
-    search_fields = ('tittle',)
+    list_display = ('title', 'update_date')
+    ordering = ('title',)
+    search_fields = ('title',)
 
 admin.site.register(Page, PageAdmin)
